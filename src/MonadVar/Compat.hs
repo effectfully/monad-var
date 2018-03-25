dc2fc7bfb64ca75b83b58f12c9edacbaee3b7c78
effectfully <effectfully@gmail.com>
2018-03-25 16:20:38 +0300

    really buildable by older 'base's

--- src/MonadVar/Compat.hs
+++ src/MonadVar/Compat.hs
@@ -2,7 +2,7 @@
 module MonadVar.Compat (module Export) where
 
 #if MIN_VERSION_base(4,9,0)
-import Data.Functor.Const as Export
+import Data.Functor.Const        as Export
 #else
 import Data.Functor.Const.Compat as Export
 #endif
